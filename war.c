fa8a0fafe0fb53f56315cd5b594bbbf85c34f2d1
Dev_Laiannesouza <laiannecris47@gmail.com>
2025-11-25 16:30:24 +0000

    dadoscadastrais

--- war.c
+++ war.c
@@ -8,7 +8,7 @@
 //--- Estrutura de dados ---
 struct Territorio {
     char nome [30];
-    char corExercito [10]; 
+    char cor[10]; 
     int tropas ; 
 };
 
@@ -19,4 +19,20 @@ int main(){
     printf ("=== Cadastro de territórios ===\n\n");
 
 // Entrada dos 5 Territórios 
+for (i = 0; i < 5; i++);            
+    printf ("--- Território %d ---\n", i + 1);
+
+    printf ("Digite o nome do território (sem espaços): ");
+    scanf ("%29s", Territorios[i].nome);
+
+    printf ("Digite a cor do exécito de tropas: ");
+    sancf ("%9s", &Territorios[i].cor);
+
+    printf (" Digite a quantidade de tropas: ");
+    sancf ("%d", &Territorios[i].tropas);
+
+    printf ("\n");
+
 }
+
+// Exibição dos dados cadastrais 
