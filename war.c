a00179b4129c6bed8608dc62391da6344e2e83f5
Dev_Laiannesouza <laiannecris47@gmail.com>
2025-11-25 21:17:41 +0000

    partefinal

--- war.c
+++ war.c
@@ -19,20 +19,29 @@ int main(){
     printf ("=== Cadastro de territórios ===\n\n");
 
 // Entrada dos 5 Territórios 
-for (i = 0; i < 5; i++);            
-    printf ("--- Território %d ---\n", i + 1);
+    for (i = 0; i < 5; i++);{          
+        printf ("--- Território %d ---\n", i + 1);
 
-    printf ("Digite o nome do território (sem espaços): ");
-    scanf ("%29s", Territorios[i].nome);
+        printf ("Digite o nome do território (sem espaços): ");
+        scanf ("%29s", Territorios[i].nome);
 
-    printf ("Digite a cor do exécito de tropas: ");
-    sancf ("%9s", &Territorios[i].cor);
+        printf ("Digite a cor do exécito de tropas: ");
+        scanf ("%9s", &Territorios[i].cor);
 
-    printf (" Digite a quantidade de tropas: ");
-    sancf ("%d", &Territorios[i].tropas);
+        printf ("Digite a quantidade de tropas: ");
+        scanf ("%d", &Territorios[i].tropas);
 
-    printf ("\n");
+        printf ("\n");
+
+    }
+// Exibição dos dados cadastrais 
+printf("\n === Dados dos Territórios Cadasrados === \n"); 
+
+for (i = 0; i < 5; i++ ) {
+    printf ("\nTerritorio %d:\n", i + 1 );
+    printf ("Nome: %s\n", Territorios[i].nome);
+    printf ("Cor do exercito: %s\n" , Territorios[i].cor);
+    printf ("Tropas: %d\n ", Territorios[1].tropas);
+    }
 
 }
-
-// Exibição dos dados cadastrais 
